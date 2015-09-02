d38cd9d34864fcbfa05f5b0eb3d99d2d8cbb2682
Juan Pablo Barreto <juampi92@gmail.com>
2015-09-02 19:39:13 -0300

    Travis CI & deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+echo "Listing directory"
+ls
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "juampi92@gmail.com"
+
+git add build/ docs/
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
