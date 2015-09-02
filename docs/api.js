56308515e1a08a6075257716191df19255ea6f1b
Travis CI <juampi92@gmail.com>
2015-09-02 23:00:51 +0000

    Deploy to GitHub Pages

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "HyperlapsePoint",
+        null
+    ],
+    "modules": [
+        "Hyperlapse"
+    ],
+    "allModules": [
+        {
+            "displayName": "Hyperlapse",
+            "name": "Hyperlapse"
+        }
+    ],
+    "elements": []
+} };
+});
